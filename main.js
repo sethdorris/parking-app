edb87afefcbe43573566a7d92f300559a2967cf6
Seth <sethc.dorris@gmail.com>
2015-07-27 13:03:10 -0500

    added success button and resizing

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ var geocoder = new google.maps.Geocoder();
 
 function codeAddress() {
     var address = document.getElementById("sellAddress").value;
+    var sellbtn = document.getElementById("sellSubmit");
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
@@ -19,6 +20,7 @@ function codeAddress() {
             map: map,
             position: results[0].geometry.location
         });
+        sellbtn.innerHTML = "Success!"
         marker.setMarker(map);
       } else {
         alert("Geocode was not successful for the following reason: " + status);
@@ -34,11 +36,16 @@ function codeAddress() {
 var find = document.getElementById("find");
 var closeFindMenuButton = document.getElementById("closeFindMenu");
 
+
+//Function to show the FIND MENU when the Find Link is clicked
+
 function findClick() {
   var Menu = document.getElementById("findMenu");
 
   Menu.style.visibility = "visible";
 }
+
+//Function to CLOSE the FIND MENU when the FIND MENU CLOSE button is clicked
 
 function closeFindMenu() {
   var Menu = document.getElementById("findMenu");
@@ -46,28 +53,47 @@ function closeFindMenu() {
   Menu.style.visibility = "hidden";
 }
 
+//Adding Event Listener to the FIND LINK
 find.addEventListener('click', findClick);
+//Adding Event Listener to the FIND MENU CLOSE BUTTON
 closeFindMenuButton.addEventListener('click', closeFindMenu);
 
 //Sell a spot button functionality
 var sellLink = document.getElementById("sell");
 var closeSellMenuButton = document.getElementById("closeSellMenu");
 
+
+//Function that opens the SELL MENU when the Sell Link is clicked
 function sellClick() {
   var sellerMenu = document.getElementById("sellMenu");
 
   sellerMenu.style.visibility = "visible";
 }
 
+//Adds Event Listener to the SELL LINK 
 sellLink.addEventListener('click', sellClick)
 
 function closeSellMenu() {
   var sellerMenu = document.getElementById("sellMenu");
+  var sellbtn = document.getElementById("sellSubmit");
+  var inputs = document.getElementsByTagName("input");
 
+  setTimeout(function() {
+    sellerMenu.style.visibility = "hidden";
+    sellbtn.innerHTML = "Sell This Spot!";
+      for (var i = 0; i < inputs.length; i++) {
+        inputs[i].value = "";
+      }
+  }, 1500)
+}
+
+function escapeSellMenu () {
+  var sellerMenu = document.getElementById("sellMenu");
   sellerMenu.style.visibility = "hidden";
 }
 
-closeSellMenuButton.addEventListener('click', closeSellMenu);
+
+closeSellMenuButton.addEventListener('click', escapeSellMenu);
 
 
   var sellBtn = document.getElementById("sellSubmit");
